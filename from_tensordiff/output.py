820e95121f33c0e8cc9f1f3a6873424447c7da01
cmi-kamalos <56527506+cmi-kamalos@users.noreply.github.com>
2022-08-18 03:08:52 +0100

    Update output.py

--- from_tensordiff/output.py
+++ from_tensordiff/output.py
@@ -4,7 +4,7 @@ import sys
 
 def print_screen(model, discovery_model=False):
     f = Figlet(font='slant')
-    print(f.renderText('TensorDiffEq'))
+    print(f.renderText('Pinn'))
     if discovery_model:
         print("Running Discovery Model for Parameter Estimation\n\n")
     print("Neural Network Model Summary\n")
