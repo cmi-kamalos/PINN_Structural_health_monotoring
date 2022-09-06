ce7ba148904163077be1e603ed86e4e46e4ebb42
Choueb Mahamat <56527506+cmi-kamalos@users.noreply.github.com>
2022-09-06 23:31:09 +0100

    update for beam plotting

--- from_tensordiff/plotting.py
+++ from_tensordiff/plotting.py
@@ -207,3 +207,128 @@ def plot_discovery(model, scale = 1):
     plt.xlabel(model.domain.domain_ids[1])
     plt.ylabel(model.domain.domain_ids[0])
     plt.show()
+def plot_solution_Beam(model, domain, ub, lb, Exact_u=None, u_transpose=False):
+    """
+    Plot a 1D solution Domain
+    Arguments
+    ---------
+    model : model
+        a `model` class which contains the PDE solution
+    domain : Domain
+        a `Domain` object containing the x,t pairs
+    ub: list
+        a list of floats containing the upper boundaries of the plot
+    lb : list
+        a list of floats containing the lower boundaries of the plot
+    Exact_u : list
+        a list of the exact values of the solution for comparison
+    u_transpose : Boolean
+        a `bool` describing whether or not to transpose the solution plot of the domain
+    Returns
+    -------
+    None
+    """
+    X, Y = np.meshgrid(domain[0],domain[1])
+
+    X_star = np.hstack((X.flatten()[:,None], Y.flatten()[:,None]))
+    if Exact_u is not None:
+        u_star= Exact_u.T.flatten()[:,None]
+
+    u_pred, f_u_pred = model.predict(X_star)
+    if u_transpose:
+        U_pred = griddata(X_star, u_pred.T.flatten(), (X, Y), method='cubic')
+    else:
+        U_pred = griddata(X_star, u_pred.flatten(), (X, Y), method='cubic')
+
+    fig, ax = newfig(1.3, 1.0)
+
+    ax.axis('off')
+
+    ####### Row 0: h(t,x) ##################
+    gs0 = gridspec.GridSpec(1, 2)
+    gs0.update(top=1-0.06, bottom=1-1/3, left=0.15, right=0.85, wspace=0)
+    ax = plt.subplot(gs0[:, :])
+
+    h = ax.imshow(U_pred.T, interpolation='nearest', cmap='YlGnBu',
+                  extent=[lb[1], ub[1], lb[0], ub[0]],
+                  origin='lower', aspect='auto')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    fig.colorbar(h, cax=cax)
+    len_ = len(domain[1])//4
+
+    line = np.linspace(domain[0].min(), domain[0].max(), 2)[:,None]
+    ax.plot(domain[1][len_]*np.ones((2,1)), line, 'k--', linewidth = 1)
+    ax.plot(domain[1][2*len_]*np.ones((2,1)), line, 'k--', linewidth = 1)
+    ax.plot(domain[1][3*len_]*np.ones((2,1)), line, 'k--', linewidth = 1)
+
+#     ax.set_xlabel('t')
+    ax.set_ylabel('x')
+    leg = ax.legend(frameon=False, loc = 'best')
+    #    plt.setp(leg.get_texts(), color='w')
+    ax.set_title(r'$\hat{w}$(x)', fontsize = 10)
+
+    ####### Row 1: h(t,x) slices ##################
+    gs1 = gridspec.GridSpec(1, 3)
+    gs1.update(top=1-1/3, bottom=0, left=0.1, right=0.9, wspace=0.5)
+
+    ax = plt.subplot(gs1[0, 0])
+    print("Exact_u",Exact_u.shape,"domain",domain[0].shape,U_pred.shape)
+    # print("Exact_u1",Exact_u[:,len_],len_)
+    ax.plot(domain[0],Exact_u[:,len_], 'b-', linewidth = 2, label = 'Exact')
+    ax.plot(domain[0],U_pred[len_,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.set_xlabel('x')
+    ax.set_ylabel('w(x)')
+    ax.set_title('s = %.2f' % (domain[1][len_]), fontsize = 10)
+    ax.axis('square')
+    ax.set_xlim([-1.1,1.1])
+    ax.set_ylim([-1.1,1.1])
+    
+
+    ax = plt.subplot(gs1[0, 1])
+    ax.plot(domain[0],Exact_u[:,2*len_], 'b-', linewidth = 2, label = 'Exact')
+    ax.plot(domain[0],U_pred[2*len_,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.set_xlabel('x')
+    ax.set_ylabel('w(x)')
+    ax.axis('square')
+    ax.set_xlim([-1.1,1.1])
+    ax.set_ylim([-1.1,1.1])
+    ax.set_title('s = %.2f' % (domain[1][2*len_]), fontsize = 10)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.3), ncol=5, frameon=False)
+
+    ax = plt.subplot(gs1[0, 2])
+    # print("exact_3",Exact_u[:,3*len_],3*len_)
+    ax.plot(domain[0],Exact_u[:,3*len_], 'b-', linewidth = 2, label = 'Exact')
+    ax.plot(domain[0],U_pred[3*len_,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.set_xlabel('x')
+    ax.set_ylabel('w(x)')
+    ax.axis('square')
+    ax.set_xlim([-1.1,1.1])
+    ax.set_ylim([-1.1,1.1])
+    ax.set_title('s = %.2f' % (domain[1][3*len_]), fontsize = 10)
+    plt.savefig("beam_pinn.png")
+    plt.show()
+    
+def plot_beam(y,x,z,ub, lb):
+    # x_plot =tf.squeeze(x,[1])
+    # t_plot =tf.squeeze(t,[1])
+    X,Y= tf.meshgrid(x,y)
+    F_xt = z
+    print(X.shape,T.shape,F_xt.shape)
+    fig,ax=plt.subplots(1,1)
+    fig.set_figheight(2)
+    fig.set_figwidth(7)
+    cp = ax.contourf(X,Y, F_xt,30,cmap="YlGnBu")
+    line = np.linspace(y.min(), y.max(), 2)[:,None]
+    len_ = len(x)//3
+    ax.plot(x[len_]*np.ones((2,1)), line, 'k--', linewidth = 1)
+    ax.plot(x[2*len_]*np.ones((2,1)), line, 'k--', linewidth = 1)
+    ax.plot(x[3*len_]*np.ones((2,1)), line, 'k--', linewidth = 1)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    fig.colorbar(cp,cax) # Add a colorbar to a plot
+    ax.set_title('w(x,y)')
+#     ax.set_xlabel('t')
+    ax.set_ylabel('x')
+    plt.savefig("original_beam_pinn.png")
+    plt.show()
